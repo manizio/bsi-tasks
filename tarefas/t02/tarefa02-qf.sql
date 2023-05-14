d7f8ae89c6f3a7b945012682ac28bd1e0f3b4c12
Manoel Anízio <92751858+manizio@users.noreply.github.com>
2023-05-14 17:47:59 -0300

    Update tarefa02-qf.sql #2

--- tarefas/t02/tarefa02-qf.sql
+++ tarefas/t02/tarefa02-qf.sql
@@ -2,9 +2,18 @@ CREATE OR REPLACE PROCEDURE show_project_members(id INT)
 	LANGUAGE plpgsql
 	AS 
 	$$
+		DECLARE 
+			res RECORD;
 		BEGIN
-			SELECT f.nome, d.sigla FROM funcionario f, departamento d, projeto p
-			WHERE f.
-			;
+			FOR RES IN SELECT f.nome, d.sigla FROM projeto p
+						JOIN equipe e ON p.equipe = e.codigo
+						JOIN membro m ON m.codequipe = e.codigo
+						JOIN funcionario f ON f.codigo = m.codfuncionario
+						JOIN departamento d ON f.depto = d.codigo
+						WHERE p.codigo = id
+			LOOP
+			RAISE NOTICE 'Funcionário: %, Departamento: %', res.nome, res.sigla;
+			END LOOP;
 		END;
 	$$;
+	
