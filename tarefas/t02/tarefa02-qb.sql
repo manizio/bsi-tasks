7ebc42768d90c9236bd4729777489093833df40e
Manoel Anízio <92751858+manizio@users.noreply.github.com>
2023-05-14 15:52:29 -0300

    Update tarefa02-qb.sql #2

--- tarefas/t02/tarefa02-qb.sql
+++ tarefas/t02/tarefa02-qb.sql
@@ -1,11 +1,12 @@
 CREATE OR REPLACE FUNCTION get_average_age()
-	returns table(departamento VARCHAR, media NUMERIC)
+	returns table(id INT,departamento VARCHAR, media NUMERIC)
 	language plpgsql
 	as $$
 	begin
-		RETURN QUERY SELECT d.sigla as departamento, AVG(get_age(f.datanasc)) as media
+		RETURN QUERY SELECT d.codigo as id, d.sigla as departamento, AVG(get_age(f.datanasc)) as media
 		from funcionario f, departamento d
 		WHERE f.codigo = d.codigo
-		GROUP BY d.codigo;
+		GROUP BY d.codigo
+		ORDER BY d.codigo;
 	end;
 	$$;
